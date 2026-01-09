6da4c2d2619140f22acf40d4eef24e3e72fc7e82
hoon3948 <robot3949@gmail.com>
2026-01-09 15:02:45 +0900

    step2: 계산기 클래스 적용, 반환 메서드 구현

--- src/step2/App.java
+++ src/step2/App.java
@@ -1,4 +1,40 @@
 package step2;
 
-public class Main {
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Calculator!");
+        Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+
+        while (true) {
+            System.out.print("계산할 부호를 입력하세요( +, -, *, /, % ): ");
+            String sign = sc.nextLine();
+
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+
+            int result = calculator.sum(num1, num2, sign);
+
+            String emptybox = sc.nextLine();
+            // enter 제거용
+
+            System.out.print("더 계산하시겠습니까?(exit입력시 종료) ");
+            String word = sc.nextLine();
+
+            if (word.equals("exit")) {
+                System.out.println("계산을 종료합니다...");
+                break;
+            }
+
+        }
+
+    }
+
 }
--- src/step2/Calculator.java
+++ src/step2/Calculator.java
@@ -2,6 +2,43 @@ package step2;
 
 public class Calculator {
 
+    boolean[][] board = new boolean[20][4];
+
+    int sum (int num1, int num2, String sign) {
+        int result = 0;
+
+        switch (sign) {
+            case "+":
+                result = num1 + num2;
+                System.out.println("결과: " + result);
+                break;
+            case "-":
+                 result = num1 - num2;
+                 System.out.println("결과: " + result);
+                 break;
+            case "*":
+                result = num1 * num2;
+                System.out.println("결과: " + result);
+                break;
+            case "/":
+                result = num1 / num2;
+                System.out.println("결과: " + result);
+                break;
+            case "%":
+                result = num1 % num2;
+                System.out.println("결과: " + result);
+                break;
+            default:
+                System.out.println("부호가 잘못 입력 되었습니다.");
+        }
+        return result;
+    }
+
+
+
+
+
+
 
 
 }
